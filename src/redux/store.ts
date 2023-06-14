c0e2e385cb4e371d90b2a7519e4b4ff9fbd48f6c
NazarYanchyshyn <yanchishin12@ukr.net>
2023-06-14 17:23:24 +0300

    fix url

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -3,11 +3,13 @@ import overviewSlice from './overview'
 import userAssetsSlice from './userAssets'
 import tableParamsSlice from './tableParams'
 import loadingSlice from './loading'
+import appChainSlice from './appChain'
 export const store = configureStore({
   reducer: {
     loadingReducer: loadingSlice,
     overviewReducer: overviewSlice,
     userAssetsReducer: userAssetsSlice,
     tableParamsReducer: tableParamsSlice,
+    appChainReducer: appChainSlice,
   },
 })
